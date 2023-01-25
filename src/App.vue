875adffe44ef52840b4c6065563564d4a6e04937
anasilva <anacmas@gmail.com>
2023-01-25 15:16:05 -0300

    feat: change header to github finder

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,23 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h1>Github Finder</h1>
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/anacmas/github_vuewer"
         target="_blank"
         text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        ><v-icon>mdi-github</v-icon>
+      </v-btn>
+
+      <v-btn
+        href="https://www.linkedin.com/in/ana-carolina-milos-17259787/"
+        target="_blank"
+        text
+        ><v-icon>mdi-linkedin</v-icon>
       </v-btn>
     </v-app-bar>
 
